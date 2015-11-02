b07a61027cd5de9209fa77239a613ec7398a526a
Jose Uusitalo <joseu.git@gmail.com>
2015-11-02 13:52:53 +0000

    Worksheet 7: All done except the final task

--- labs/07 JavaScript Tools/js/contact.js
+++ labs/07 JavaScript Tools/js/contact.js
@@ -1,32 +1,49 @@
 // contact.js
 // This script checks a contact form submission for HTML and a valid email address.
-
 // Function called when the form is submitted.
 // Function validates the data and returns a Boolean. (Logs to console in this example)
+/*global alert */
+var document;
+var console;
+var window;
+
 function process() {
     'use strict';
-
     // Variable to represent validity:
+    var i;
     var okay = true;
-    
+    var nameOK = true;
     // Get form references:
+    var sFullName = document.getElementById('fullName');
     var email = document.getElementById('email');
     var comments = document.getElementById('comments');
+    // Split name into array.
+    var parts = sFullName.value.split(" ");
+    console.log("parts: " + parts + " | " + parts.length);
+    // Check for double and trailing spaces.
+    for(i = 0; i < parts.length; i = +1) {
+        if(!parts[i]) {
+            console.log("Empty name part!");
+            nameOK = false;
+            break;
+        }
+    }
+    // Validate the name:
+    if(!sFullName || !sFullName.value || (sFullName.value.length < 3) || (sFullName.value.length > 100) || (sFullName.value.indexOf(' ') === -1) || parts.length < 2 || !nameOK) {
+        okay = false;
+        alert('Please enter your first name and last name separated with a space! (Must be between 3-100 characters long and must not contain double or trailing spaces..)');
+    }
     // Validate the email address:
-    if (!email || !email.value 
-    || (email.value.length < 6) 
-    || (email.value.indexOf('@') == -1)) {
+    if(!email || !email.value || (email.value.length < 6) || (email.value.indexOf('@') === -1)) {
         okay = false;
         alert('Please enter a valid email address!');
     }
-
     // Validate the comments:
     var maxLength = 100;
-    if (!comments || !comments.value 
-    || (comments.value.indexOf('<') != -1) ) {
+    if(!comments || !comments.value || (comments.value.indexOf('<') !== -1)) {
         okay = false;
         alert('Please enter your comments, without any HTML!');
-    } else if (comments.value.length > maxLength) {
+    } else if(comments.value.length > maxLength) {
         okay = false;
         var originalText = comments.value;
         // Find the last space before the limit:
@@ -35,31 +52,27 @@ function process() {
         var limitedText = originalText.slice(0, lastSpace);
         comments.value = limitedText;
         okay = true;
-
         alert('Comment was trimmed under ' + maxLength + ' characters');
     }
-        
     // Normally you would "return okay;" here to submit/block the form submission
     // return okay;
-
     // For this example we alert the user and log details to the console
     var message;
-    if (okay) {
+    if(okay) {
         message = "Form submitted";
     } else {
         message = "Form not submitted";
     }
     console.log(message);
+    console.log("Full Name: " + sFullName.value);
     console.log("Email: " + email.value);
     console.log("Comments: " + comments.value);
     alert(message);
-
     // Prevent submission for the purposes of this example:
     return false;
-    
 } // End of process() function.
-
 // Initial setup:
+
 function init() {
     'use strict';
     document.getElementById('theForm').onsubmit = process;
--- labs/07 JavaScript Tools/js/notes.js
+++ labs/07 JavaScript Tools/js/notes.js
@@ -1,33 +1,66 @@
-
-var notes = new Array();
-
-function addItem() {
-	textbox = document.getElementById('item');
-	var itemText = textbox.value;
-	textbox.value = '';
-	textbox.focus();
-	var newItem = {title: itemText, quantity: 1};
-	notes.push(newItem);
-	displayList()
-}
+"use strict";
+var notes = [];
+var textbox;
+var document;
+var button;
+var console;
+var localStorage;
 
 function displayList() {
-	var table = document.getElementById('list');
-	table.innerHTML = '';
-	for (var i = 0; i<notes.length; i++) {
-		var node = undefined;
-		var note = notes[i];
-		var node = document.createElement('tr');
-		var html = '<td>'+note.title+'</td><td>'+note.quantity+'</td><td><a href="#" onClick="deleteIndex('+i+')">delete</td>';
-	    node.innerHTML = html;
-		table.appendChild(node);
-	}
+    var table = document.getElementById('list');
+    var i;
+    var html;
+    var node;
+    var note;
+    table.innerHTML = '';
+    for(i = 0; i < notes.length; i += 1) {
+        note = notes[i];
+        node = document.createElement('tr');
+        html = '<td>' + note.title + '</td><td>' + note.quantity + '</td><td><a href="#" onClick="deleteIndex(' + i + ')">delete</td>';
+        node.innerHTML = html;
+        table.appendChild(node);
+    }
+}
+
+function addItem() {
+    textbox = document.getElementById('item');
+    var itemText = textbox.value;
+    var old = false;
+    textbox.value = '';
+    textbox.focus();
+    var i;
+    for(i = 0; i < notes.length; i += 1) {
+        if(notes[i].title === itemText) {
+            notes[i].quantity += 1;
+            old = true;
+            break;
+        }
+    }
+    if(!old) {
+        var newItem = {
+            title: itemText,
+            quantity: 1
+        };
+        notes.push(newItem);
+    }
+    displayList();
 }
 
 function deleteIndex(i) {
-	notes.splice(i, 1);
-	displayList();
+    notes.splice(i, 1);
+    displayList();
 }
 
-button = document.getElementById('add')
+function saveList() {
+    localStorage.notes = JSON.stringify(notes);
+}
+
+function loadList() {
+    console.log('loadList');
+    if(localStorage.notes) {
+        notes = JSON.parse(localStorage.notes);
+        displayList();
+    }
+}
+button = document.getElementById('add');
 button.onclick = addItem;
