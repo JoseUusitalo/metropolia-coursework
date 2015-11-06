3cef582d093d7554f9b844299abdb4b30fb68812
Jose Uusitalo <joseu.git@gmail.com>
2015-11-06 09:42:49 +0000

    Worksheet 10: Done

--- labs/10 Single-Page Applications/js/building_lists.js
+++ labs/10 Single-Page Applications/js/building_lists.js
@@ -1,17 +1,57 @@
-
+"use strict";
+var console, document, i, row, title, year, isbn, author, sectionHead, sectionBody;
 var request = new XMLHttpRequest();
 request.open('GET', 'data/books.json', false);
 request.send(null);
 var data = JSON.parse(request.responseText);
-console.log(data);
-
 var books = data.books;
+var table = document.createElement('table');
+for (i = -1; i < books.length; i += 1) {
+	row = document.createElement("tr");
+	row.onclick = function() {
+		var p = document.querySelector("p");
+		if (!p) {
+			p = document.createElement("p");
+			var div = document.createElement("div");
+			div.appendChild(p);
+			document.body.insertBefore(div, document.body.firstChild);
+		}
+		p.innerHTML = this.childNodes[0].innerHTML;
 
-var list = document.createElement('ol');
-for (var i=0; i < books.length; i++) {
-	console.log(books[i].title);
-	var item = document.createElement('li');
-	item.innerHTML = books[i].title;
-	list.appendChild(item);
+	};
+	if (i < 0) {
+		row = document.createElement("tr");
+		sectionHead = document.createElement("thead");
+		table.appendChild(sectionHead);
+		title = document.createElement("th");
+		title.innerHTML = "Title";
+		row.appendChild(title);
+		year = document.createElement("th");
+		year.innerHTML = "Year";
+		row.appendChild(year);
+		isbn = document.createElement("th");
+		isbn.innerHTML = "ISBN";
+		row.appendChild(isbn);
+		author = document.createElement("th");
+		author.innerHTML = "Authors";
+		row.appendChild(author);
+		sectionHead.appendChild(row);
+		sectionBody = document.createElement("tbody");
+		table.appendChild(sectionBody);
+	} else {
+		title = document.createElement("td");
+		title.innerHTML = books[i].title;
+		row.appendChild(title);
+		year = document.createElement("td");
+		year.innerHTML = books[i].year;
+		row.appendChild(year);
+		isbn = document.createElement("td");
+		isbn.innerHTML = books[i].isbn;
+		row.appendChild(isbn);
+		author = document.createElement("td");
+		author.innerHTML = books[i].authors;
+		row.appendChild(author);
+		sectionBody.appendChild(row);
+	}
 }
-document.body.appendChild(list);
+document.body.appendChild(table);
--- labs/10 Single-Page Applications/js/simple_dom.js
+++ labs/10 Single-Page Applications/js/simple_dom.js
@@ -1,24 +1,13 @@
-//console.log('page loaded');
-
 console.log(document);
-
-document.getElementById('save').onclick = save;
-document.querySelector('#userForm input[type="email"]').onkeypress = function() {
-	console.log('updating email');
-	var email = document.querySelector('#userForm input[type="email"]').value;
-	document.querySelector('#summary p').innerHTML = email;
+document.querySelector('#userForm input[type="text"]').onkeyup = function () {
+    var name = document.querySelector('#userForm input[type="text"]');
+    document.querySelector('#summary h1').innerHTML = name.value;
 };
-
-
-function save() {
-	console.log('save');
-	// get a DOM object representing a form field.
-	var name = document.querySelector('#userForm input[type="text"]');
-	console.log(name);
-	document.querySelector('#summary h1').innerHTML = name.value;
-	var data = document.querySelectorAll('#userForm input');
-	console.log(data);
-	var paragraphs = document.querySelectorAll('#summary p');
-	console.log('found '+paragraphs.length+' p tags');
-	paragraphs[1].innerHTML = 'Hello World!';
-}
+document.querySelector('#userForm input[type="password"]').onkeyup = function () {
+    var password = document.querySelector('#userForm input[type="password"]');
+    document.querySelectorAll('#summary p')[1].innerHTML = password.value;
+};
+document.querySelector('#userForm input[type="email"]').onkeyup = function () {
+    var email = document.querySelector('#userForm input[type="email"]').value;
+    document.querySelector('#summary p').innerHTML = email;
+};
